0a65a6cf5fcf7b8a75640343cbf06e5b993f9cf6
EmmanuelNdo <137494060+EmmanuelNdo@users.noreply.github.com>
2023-07-21 11:58:42 +0200

    dev: mail.ts récupéré depuis github jlg

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,24 +1,43 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
+import * as echarts from "echarts";
+import { queryData } from "./query";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+(async () => {
+  const chartDom = document.querySelector<HTMLElement>("div.map");
+  if (chartDom === null) {
+    throw new Error("Cannot find div.map");
+  }
+  const myChart = echarts.init(chartDom);
+  console.log("myChart: ", myChart);
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  const response = await fetch("/indonesia.geojson");
+  const json = await response.json();
+  console.log("json: ", json);
+
+  const wikidataRequest = `
+  SELECT ?item ?itemLabel ?population ?code ?surface  {
+    ?item wdt:P31/wdt:P279* wd:Q5098.
+    ?item wdt:P1082 ?population.
+    ?item wdt:P300 ?code.
+    ?item wdt:P2046 ?surface
+    SERVICE wikibase:label { bd:serviceParam wikibase:language "fr,en" }
+  } ORDER BY DESC(?item)
+  `;
+  console.log("wikidataRequest: ", wikidataRequest);
+
+  const populationJson = await queryData(wikidataRequest);
+  console.log("populationJson: ", populationJson);
+
+  const results = populationJson.results.bindings;
+  console.log("results: ", results);
+
+  const popJson = results.map((row: any) => {
+    return {
+      code: row.code.value,
+      surface: row.surface.value,
+      population: row.population.value,
+      label: row.itemLabel.value,
+    };
+  });
+  console.log("popJson: ", popJson);
+})();
